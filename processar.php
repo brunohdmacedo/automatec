218ac4eabd305117663a6986d64b06e6c070c3e6
Bruno Macedo <brunohenriquedourado@gmail.com>
2023-10-01 18:14:06 -0300

    Update processar.php

--- processar.php
+++ processar.php
@@ -2,6 +2,8 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $email = $_POST["email"];
+    $curso = $_POST["curso"];
+    $matricula = $_POST["matricula"];
     $descricao = $_POST["descricao"];
     $curriculo = $_POST["curriculo"];
 
@@ -9,6 +11,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $assunto = 'Novo Cadastro de Membro';
     $mensagem = "Nome: $nome\n";
     $mensagem .= "E-mail: $email\n";
+    $mensagem .= "Curso: $curso\n";
+    $mensagem .= "Matrícula: $matricula\n";
     $mensagem .= "Descrição: $descricao\n";
     $mensagem .= "Currículo: $curriculo\n";
 
@@ -16,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $destinatario = 'bruno@brunohdmacedo.engineer';
 
     // Enviar e-mail
-    $headers = 'From: webmaster@example.com'; // Substitua pelo seu endereço de e-mail
+    $headers = 'From: bruno@brunohdmacedo.engineer'; // Substitua pelo seu endereço de e-mail
     if(mail($destinatario, $assunto, $mensagem, $headers)) {
         echo "Obrigado, $nome! Seu formulário foi enviado com sucesso.";
     } else {
