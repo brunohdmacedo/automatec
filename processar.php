d725cce132fb35b516816c2ed93b3fb9f554d394
Bruno Macedo <brunohenriquedourado@gmail.com>
2023-10-01 18:00:22 -0300

    Update processar.php

--- processar.php
+++ processar.php
@@ -5,27 +5,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $descricao = $_POST["descricao"];
     $curriculo = $_POST["curriculo"];
 
-    // Conectar ao banco de dados (substitua pelos seus próprios detalhes de conexão)
-    $conn = new mysqli("localhost", "downlo32_bhs", "Bhs@1536", "downlo32_uni");
+    // Formatar os dados como uma linha CSV
+    $linha_csv = "$nome, $email, $descricao, $curriculo\n";
 
-    // Verificar a conexão
-    if ($conn->connect_error) {
-        die("Erro de conexão: " . $conn->connect_error);
-    }
+    // Salvar os dados em um arquivo CSV
+    file_put_contents("dados.csv", $linha_csv, FILE_APPEND);
 
-    // Preparar a instrução SQL para inserção
-    $stmt = $conn->prepare("INSERT INTO membros (nome, email, descricao, curriculo) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $nome, $email, $descricao, $curriculo);
-
-    // Executar a instrução SQL
-    if ($stmt->execute()) {
-        echo "Obrigado, $nome! Recebemos seus dados. Em breve entraremos em contato!";
-    } else {
-        echo "Erro ao enviar dados. Por favor, tente novamente mais tarde.";
-    }
-
-    // Fechar a conexão e a instrução SQL
-    $stmt->close();
-    $conn->close();
+    // Resposta para o cliente (pode ser usada para exibir uma mensagem na página)
+    echo "Obrigado, $nome! Recebemos seus dados. Em breve entraremos em contato!";
 }
 ?>
