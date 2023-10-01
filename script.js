6e883de21da62e2d4f5dca7a0fdcfa744a704fc3
Bruno Macedo <brunohenriquedourado@gmail.com>
2023-10-01 17:45:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ document.getElementById('form').addEventListener('submit', function (event) {
 
     var nome = document.getElementById('nome').value;
     var email = document.getElementById('email').value;
+    var descricao = document.getElementById('descricao').value;
+    var curriculo = document.getElementById('curriculo').value;
 
-    // Simulando envio de dados para o servidor usando AJAX
-    setTimeout(function () {
-        var respostaDiv = document.getElementById('resposta');
-        respostaDiv.innerHTML = `<p>Obrigado, ${nome}! Recebemos seu e-mail (${email}). Em breve entraremos em contato!</p>`;
-        respostaDiv.style.backgroundColor = '#4caf50';
-        respostaDiv.style.color = 'white';
-        respostaDiv.style.padding = '20px';
-        respostaDiv.style.borderRadius = '10px';
-        respostaDiv.style.marginTop = '20px';
-    }, 1500);
+    // Envia dados para o servidor usando AJAX
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'processar.php', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var respostaDiv = document.getElementById('resposta');
+            respostaDiv.innerHTML = `<p>${xhr.responseText}</p>`;
+            respostaDiv.style.backgroundColor = '#4caf50';
+            respostaDiv.style.color = 'white';
+            respostaDiv.style.padding = '20px';
+            respostaDiv.style.borderRadius = '10px';
+            respostaDiv.style.marginTop = '20px';
+        }
+    };
+    xhr.send(`nome=${nome}&email=${email}&descricao=${descricao}&curriculo=${curriculo}`);
 });
