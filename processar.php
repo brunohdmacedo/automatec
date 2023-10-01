8cb4feb7dd7aef53f8bc5552ff15457bd8d22930
Bruno Macedo <brunohenriquedourado@gmail.com>
2023-10-01 17:26:23 -0300

    Create processar.php

---
+++ processar.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+
+    // Aqui você pode processar os dados, enviar e-mails, armazenar no banco de dados, etc.
+    // Neste exemplo, não estamos fazendo nada com os dados recebidos.
+}
+?>
